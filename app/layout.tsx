e9ff9088e009baa337bf2deba75a66949acfd245
kennethalam <kennethalamx7@gmail.com>
2026-02-06 11:39:37 -0500

    Remove Tawk.to widget

--- app/layout.tsx
+++ app/layout.tsx
@@ -236,24 +236,6 @@ export default function RootLayout({
           </LanguageProvider>
         </ThemeProvider>
 
-        {/* Tawk.to Live Chat Widget */}
-        <Script
-          id="tawk-to-chat"
-          strategy="lazyOnload"
-          dangerouslySetInnerHTML={{
-            __html: `
-              var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-              (function(){
-                var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-                s1.async=true;
-                s1.src='https://embed.tawk.to/696eb05d1090e9198185f007/1jfc61r4d';
-                s1.charset='UTF-8';
-                s1.setAttribute('crossorigin','*');
-                s0.parentNode.insertBefore(s1,s0);
-              })();
-            `
-          }}
-        />
       </body>
     </html>
   );
